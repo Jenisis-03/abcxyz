60ffe3be7db93b578b2a3016a4745bad60dae027
Subham Dubey <111039096+Jenisis-03@users.noreply.github.com>
2024-10-16 22:32:34 +0530

    Your descriptive commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import {
   DocumentTextIcon,
